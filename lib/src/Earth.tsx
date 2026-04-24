8ffec2f592a4c7779c23d35928c2d6ea16107a9a
Clara Ni <clara.ni@outlook.fr>
2026-04-24 17:39:57 +0900

    [lib] tech: introduce useSvgController
    Group in a custom hook logic related to svg.
    
    Signed-off-by: Clara Ni <clara.ni@outlook.fr>

--- lib/src/Earth.tsx
+++ lib/src/Earth.tsx
@@ -1,14 +1,10 @@
-import { useEffect, useLayoutEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
 
-import * as d3 from "d3";
-import { geoPath, type GeoPermissibleObjects, type GeoSphere } from "d3-geo";
-import type { FeatureCollection, Geometry, LineString } from "geojson";
+import type { FeatureCollection, Geometry } from "geojson";
 
-import { createProjection } from "./utils/projections";
+import useSvgController from "./useSvgController";
 import { useView } from "./utils/view";
 import "./styles.css";
-
-const SPHERE: GeoSphere = { type: "Sphere" };
 
 type EarthProps = {
   coastlines?: FeatureCollection<Geometry>;
@@ -17,81 +13,24 @@ type EarthProps = {
 const Earth = ({ coastlines }: EarthProps) => {
   const earthRoot = useRef<HTMLDivElement | null>(null);
   const globeSvgRef = useRef<SVGSVGElement | null>(null);
-  const projectionRef = useRef<d3.GeoProjection | null>(null);
 
   const view = useView(earthRoot);
 
-  useLayoutEffect(() => {
-    const svg = globeSvgRef.current;
-    if (!svg) return;
-
-    // **********************
-    // * SVG initialization *
-    // **********************
-    const globeSvg = d3.select<SVGSVGElement, unknown>(svg);
-    globeSvg.selectAll("*").remove();
-
-    // Sphere
-    globeSvg.append("path").datum(SPHERE).attr("fill", "#0a0a0a");
-
-    // Coastlines
-    globeSvg
-      .append("path")
-      .attr("class", "coastlines")
-      .datum(coastlines)
-      .attr("fill", "none")
-      .attr("stroke", "white")
-      .attr("stroke-width", 1);
-
-    // Graticules (long/lat grid) and equator line
-    const graticule = d3.geoGraticule();
-    globeSvg
-      .append("path")
-      .datum(graticule())
-      .attr("fill", "none")
-      .attr("stroke", "rgba(200,200,200,0.4)")
-      .attr("stroke-width", 0.5);
-
-    const equator: LineString = {
-      type: "LineString",
-      coordinates: d3.range(-180, 181, 1).map((lon) => [lon, 0]),
-    };
-    globeSvg
-      .append("path")
-      .datum(equator)
-      .attr("fill", "none")
-      .attr("stroke", "#707070")
-      .attr("stroke-width", 1.25);
-
-    // **************
-    // * SVG update *
-    // **************
-    const updateSvg = () => {
-      const path = geoPath<SVGPathElement, GeoPermissibleObjects>().projection(
-        projectionRef.current!,
-      );
-      globeSvg
-        .selectAll<SVGPathElement, GeoPermissibleObjects>("path")
-        .attr("d", path);
-    };
-
-    projectionRef.current = createProjection(view);
-    updateSvg();
-  }, [globeSvgRef, view]);
+  const svgController = useSvgController(globeSvgRef);
 
   useEffect(() => {
-    if (!coastlines) return;
-
-    const svg = globeSvgRef.current;
-    if (!svg) return;
-
-    const globeSvg = d3.select(svg);
-
-    globeSvg
-      .select(".coastlines")
-      .datum(coastlines)
-      .attr("d", d3.geoPath().projection(projectionRef.current!));
+    if (svgController && coastlines) {
+      svgController.updateCoastlines(coastlines);
+    }
   }, [coastlines]);
+
+  // ********************
+  // * First render     *
+  // ********************
+  useEffect(() => {
+    if (!svgController) return;
+    svgController.changeProjection(view);
+  }, [svgController]);
 
   return (
     <div ref={earthRoot} className="earth-root">
